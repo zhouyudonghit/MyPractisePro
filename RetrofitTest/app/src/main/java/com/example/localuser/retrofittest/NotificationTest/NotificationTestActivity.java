1d0d1975c39a250714a2fa7c7226b48524f12bb3
zhouyudong <zhouyudong_hit@126.com>
2018-12-31 00:54:26 +0800

    add notification codes
    Signed-off-by: zhouyudong <zhouyudong_hit@126.com>

--- RetrofitTest/app/src/main/java/com/example/localuser/retrofittest/NotificationTest/NotificationTestActivity.java
+++ RetrofitTest/app/src/main/java/com/example/localuser/retrofittest/NotificationTest/NotificationTestActivity.java
@@ -1,7 +1,11 @@
 package com.example.localuser.retrofittest.NotificationTest;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -19,8 +23,19 @@ public class NotificationTestActivity extends AppCompatActivity{
         mSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                sendNotification();
             }
         });
+    }
+
+    public void sendNotification()
+    {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"1");
+        builder.setContentTitle("test notifiy").setContentText("notify content")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.content_films))
+                .setSmallIcon(R.mipmap.content_music).setWhen(System.currentTimeMillis())
+                .setTicker("set ticker").setDefaults(Notification.DEFAULT_SOUND);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1,builder.build());
     }
 }
