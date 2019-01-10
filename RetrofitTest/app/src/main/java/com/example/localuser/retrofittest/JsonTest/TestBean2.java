71ceafdcb97e50a9f799c3a01f063296cd4c5fc5
yudongzhou <yudongzhou@pptv.com>
2019-01-10 14:39:47 +0800

    add memoryleak test codes
    Signed-off-by: yudongzhou <yudongzhou@pptv.com>

---
+++ RetrofitTest/app/src/main/java/com/example/localuser/retrofittest/JsonTest/TestBean2.java
@@ -0,0 +1,13 @@
+package com.example.localuser.retrofittest.JsonTest;
+
+public class TestBean2 {
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
---
+++ RetrofitTest/app/src/main/java/com/example/localuser/retrofittest/MemoryLeak/ICallback.java
@@ -0,0 +1,5 @@
+package com.example.localuser.retrofittest.MemoryLeak;
+
+public interface ICallback {
+    void test();
+}
