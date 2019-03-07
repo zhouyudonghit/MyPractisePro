802da0e8c396dccc072bec2117a6400ab8b6798b
yudongzhou <yudongzhou@pptv.com>
2019-03-07 19:23:51 +0800

    add codes
    Signed-off-by: yudongzhou <yudongzhou@pptv.com>

--- RetrofitTest/app/src/main/java/com/example/localuser/retrofittest/OKhttpTest/OkHttpTest.java
+++ RetrofitTest/app/src/main/java/com/example/localuser/retrofittest/OKhttpTest/OkHttpTest.java
@@ -8,8 +8,10 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OkHttpTest {
@@ -36,6 +38,7 @@ public class OkHttpTest {
         });
     }
 
+    //同步Get请求
     public void test2()
     {
         String url = "http://wwww.baidu.com";
@@ -43,10 +46,39 @@ public class OkHttpTest {
         final Request request = new Request.Builder().url(url).get().build();
         Call call = httpClient.newCall(request);
         try {
+            //该过程会阻塞UI线程，所以下面代码正确的写法应该放在子线程，否则会报异常
             Response response = call.execute();
         } catch (IOException e) {
             e.printStackTrace();
             Log.d(TAG,e.getLocalizedMessage());
         }
+    }
+
+    /**
+     * 测试提交key-value对
+     */
+    public void testPostKeyValue()
+    {
+        RequestBody requestBody = new FormBody.Builder().add("username","zhouyudong")
+                .add("password","123456")
+                .build();
+
+        Request request = new Request.Builder().url("http://wwww.baidu.com")
+                .post(requestBody)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
     }
 }
