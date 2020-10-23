b306ec691156c1ea3f3206ffe14548b79a579115
yudongzhou <18103518@suning.com>
2020-10-23 21:29:37 +0800

    add codes
    Signed-off-by: yudongzhou <18103518@suning.com>

--- RetrofitTest/app/src/main/java/com/example/localuser/retrofittest/MainActivity.java
+++ RetrofitTest/app/src/main/java/com/example/localuser/retrofittest/MainActivity.java
@@ -52,6 +52,7 @@ import com.example.localuser.retrofittest.ViewpagerTest.ViewpagerTestActivity;
 import com.example.localuser.retrofittest.WebViewTest.WebViewTestActivity;
 import com.example.localuser.retrofittest.service.ServiceTestActivity;
 import com.example.localuser.retrofittest.statusbartest.StatusbarTestActivity;
+import com.example.localuser.retrofittest.stepcounter.StepCounterTestActivity;
 import com.example.localuser.retrofittest.uiutils.UIUtilsActivity;
 
 import java.util.ArrayList;
@@ -70,6 +71,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private TextView tv1,tv2,tv3,tv4,tv5,tv6,tv7,tv8,tv9,tv10,tv11,tv12,tv13,tv14,tv15,tv16,tv17,tv18,tv19,tv20,tv21,tv22,tv23;
     private TextView tv24,tv25,tv26,tv27,tv28,tv29,tv30,tv31,tv32,tv33,tv34,tv35,tv36,tv37,tv38,tv39,tv40,tv41,tv42,tv43,tv44,tv45;
+    private TextView tv46;
     public static String TAG = "retrofit";
 
     @Override
@@ -453,6 +455,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, UIUtilsActivity.class));
             }
         });
+
+        tv46 = findViewById(R.id.stepcounter_activity);
+        tv46.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StepCounterTestActivity.class));
+            }
+        });
 //        BootCompletedReceiver receiver = new BootCompletedReceiver();
 //        IntentFilter intentFilter = new IntentFilter();
 //        intentFilter.addAction(Intent.ACTION_BOOT_COMPLETED);
@@ -514,6 +524,7 @@ public class MainActivity extends AppCompatActivity {
 //        list.add(tv43);
         list.add(tv44);
 //        list.add(tv45);
+//        list.add(tv46);
         for(TextView textView :list)
         {
             textView.setVisibility(View.GONE);
